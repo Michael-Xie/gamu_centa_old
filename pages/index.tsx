728d8d91a34777634f5ef1628b9aeddffaa17fee
Michael-Xie <xiemicha@gmail.com>
2024-03-17 08:38:46 -0400

    feat: remove starter components to simplify start

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,8 @@
-import { Welcome } from '../components/Welcome/Welcome';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
-
 export default function HomePage() {
   return (
     <>
-      <Welcome />
-      <ColorSchemeToggle />
+      <div>Hello World</div>
+      <span>Hi!</span>
     </>
   );
 }
